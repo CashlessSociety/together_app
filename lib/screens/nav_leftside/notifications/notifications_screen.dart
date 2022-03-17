42a8db06c67238280beab4ac9c7d28b94fbf8651
Kane <mjxchina@163.com>
2022-03-17 15:38:36 +0800

    Skills Page: some fixes

--- lib/screens/nav_leftside/notifications/notifications_screen.dart
+++ lib/screens/nav_leftside/notifications/notifications_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:together_app/components/alter_scaffold.dart';
+import 'package:together_app/components/buttons.dart';
+import 'package:together_app/utils/constants.dart';
 
 class NotificationsScreen extends StatelessWidget {
   static String routeName = '/notifications_screen';
@@ -8,6 +10,11 @@ class NotificationsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlterScaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryBlue,
+        title: const Text('Notification'),
+        leading: const AppBarBackButton(),
+      ),
       body: Center(
         child: Text('This is $routeName'),
       ),
