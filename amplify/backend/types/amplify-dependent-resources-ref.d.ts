6128e060c38844f00b61e3cd4947c99b4508bce8
Kane <mjxchina@163.com>
2022-03-22 20:43:25 +0800

    Profile Page: Add support for AWS Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "togetherprojectfab8fe30": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "togetherProjectS3": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
