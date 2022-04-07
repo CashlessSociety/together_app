ba076f82c156c40d3014d91f94a43645b63a2afd
mjxchina <mjxchina@163.com>
2022-04-07 13:26:46 +0800

    Profile Header Update: Part 2

--- lib/screens/common/profile/profile_info.dart
+++ lib/screens/common/profile/profile_info.dart
@@ -1,14 +1,15 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:graphql/client.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:provider/provider.dart';
+import 'package:readmore/readmore.dart';
 import 'package:together_app/graphql/query/query.graphql.dart';
 import 'package:together_app/screens/common/gratitude/gratitude_screen.dart';
 import 'package:together_app/screens/common/profile/profile_edit_screen.dart';
@@ -44,11 +45,19 @@ class _ProfileInfoState extends State<ProfileInfo>
   ];
   final double tabHeight = 45.w;
   final double collapsedHeight = 45.w;
-  final double expandedHeight = 220.w;
+  final double expandedHeight = 230.w;
   final double indicatorWeight = 2.w;
   final double topBarHeight = 45.w;
   late double tabViewHeight;
   Function? refetchFunction;
+
+  double scrollPercentMinModifier(double scrollPercent, double minPercent) {
+    if (scrollPercent >= minPercent) {
+      return (scrollPercent - minPercent) / (1 - minPercent);
+    } else {
+      return 0;
+    }
+  }
 
   void onGotoProfileEdit() async {
     var rst = await Get.toNamed(
@@ -149,7 +158,7 @@ class _ProfileInfoState extends State<ProfileInfo>
                       } else {
                         scrollPercent = constraints.scrollOffset / scrollSpace;
                       }
-                      hasCollapsed = scrollPercent > 0.9;
+                      hasCollapsed = scrollPercent > 0.85;
                       return SliverAppBar(
                         collapsedHeight: collapsedHeight,
                         expandedHeight: expandedHeight,
@@ -255,6 +264,7 @@ class _ProfileInfoState extends State<ProfileInfo>
     bool hasCollapsed,
     QueryResult<QueryGetUserWithId> result,
   ) {
+    double halfScrollPercent = scrollPercentMinModifier(scrollPercent, 0.5);
     return FlexibleSpaceBar(
       expandedTitleScale: 1,
       centerTitle: true,
@@ -285,11 +295,18 @@ class _ProfileInfoState extends State<ProfileInfo>
           ),
 
           Positioned(
-            top: 25.w + ScreenUtil().statusBarHeight + 25.w * scrollPercent,
+            top: 25.w + ScreenUtil().statusBarHeight + 102.w,
+            left: 25.w,
+            width: 94.w,
+            child: buildGratitudeIconWithText(),
+          ),
+
+          Positioned(
+            top: 25.w + ScreenUtil().statusBarHeight + 25.w * halfScrollPercent,
             left: 25.w,
             height: 94.w,
             width: 94.w,
-            child: buildUserAvatarSection(result, scrollPercent),
+            child: buildUserAvatarSection(result, halfScrollPercent),
           ),
 
           Positioned(
@@ -297,7 +314,7 @@ class _ProfileInfoState extends State<ProfileInfo>
             left: 140.w,
             right: 25.w,
             bottom: tabHeight + indicatorWeight,
-            child: buildUserInfoSection(result),
+            child: buildUserInfoSection(result, halfScrollPercent),
           ),
 
           /// light yellow background
@@ -318,14 +335,23 @@ class _ProfileInfoState extends State<ProfileInfo>
               top: ScreenUtil().statusBarHeight,
               right: 0,
               left: 0,
-              bottom: topBarHeight,
-              child: Center(
-                child: Text(
-                  'Title',
-                  style: TextStyle(
-                    color: kPrimaryBlack,
+              height: topBarHeight + indicatorWeight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(width: 50.w),
+                  Text(
+                    result.parsedData?.getUser?.email ?? "",
+                    style: TextStyle(
+                        color: kDeepBlue2,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w400),
                   ),
-                ),
+                  SizedBox(
+                    width: 50.w,
+                    child: Center(child: buildGratitudeIcon()),
+                  ),
+                ],
               ),
             ),
         ],
@@ -333,136 +359,222 @@ class _ProfileInfoState extends State<ProfileInfo>
     );
   }
 
+  Widget buildGratitudeIcon() {
+    if (!widget.isOwner) {
+      return GestureDetector(
+        onTap: () {
+          showToast('send gratitude');
+        },
+        child: SvgPicture.asset(
+          'assets/svgs/give_gratitude_icon.svg',
+          width: 26.w,
+          height: 24.w,
+        ),
+      );
+    } else {
+      return const SizedBox();
+    }
+  }
+
+  Widget buildGratitudeIconWithText() {
+    if (!widget.isOwner) {
+      return GestureDetector(
+        onTap: () {
+          showToast('send gratitude');
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SvgPicture.asset(
+              'assets/svgs/give_gratitude_icon.svg',
+              width: 26.w,
+              height: 24.w,
+            ),
+            SizedBox(
+              height: 3.w,
+            ),
+            Text(
+              "GIVE\nGRATITUDE",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 9.sp,
+                color: kPrimaryBlack,
+                fontWeight: FontWeight.w700,
+              ),
+            )
+          ],
+        ),
+      );
+    } else {
+      return const SizedBox();
+    }
+  }
+
   Widget buildUserAvatarSection(
     QueryResult<QueryGetUserWithId> result,
     double scrollPercent,
   ) {
     String avatar = result.parsedData?.getUser?.avatar ?? "";
-    return Container(
-      clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        shape: BoxShape.circle,
-        border: Border.all(color: Colors.white, width: 3.w),
-      ),
-      child: avatar == ""
-          ? Icon(
-              Icons.person,
-              color: Colors.grey,
-              size: 50.w,
-            )
-          : Container(
-              clipBehavior: Clip.antiAlias,
-              decoration: const BoxDecoration(shape: BoxShape.circle),
-              child: CachedNetworkImage(
-                fadeInDuration: const Duration(milliseconds: 200),
-                fadeOutDuration: const Duration(milliseconds: 200),
-                imageUrl: toCdnUrl(avatar),
-                placeholder: (context, url) {
-                  return CircularProgressIndicator(
-                    strokeWidth: 2.w,
-                  );
-                },
-                errorWidget: (context, url, error) {
-                  return const Icon(Icons.error);
-                },
+    return Opacity(
+      opacity: 1 - scrollPercent,
+      child: Container(
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.white, width: 3.w),
+        ),
+        child: avatar == ""
+            ? Icon(
+                Icons.person,
+                color: Colors.grey,
+                size: 50.w,
+              )
+            : Container(
+                clipBehavior: Clip.antiAlias,
+                decoration: const BoxDecoration(shape: BoxShape.circle),
+                child: CachedNetworkImage(
+                  fadeInDuration: const Duration(milliseconds: 200),
+                  fadeOutDuration: const Duration(milliseconds: 200),
+                  imageUrl: toCdnUrl(avatar),
+                  placeholder: (context, url) {
+                    return CircularProgressIndicator(
+                      strokeWidth: 5.w,
+                      valueColor: AlwaysStoppedAnimation<Color>(kPrimaryYellow),
+                    );
+                  },
+                  errorWidget: (context, url, error) {
+                    return const Icon(Icons.error);
+                  },
+                ),
               ),
-            ),
+      ),
     );
   }
 
-  Widget buildUserInfoSection(QueryResult<QueryGetUserWithId> result) {
+  Widget buildUserInfoSection(
+    QueryResult<QueryGetUserWithId> result,
+    double scrollPercent,
+  ) {
     if (result.parsedData != null) {
       if (result.parsedData!.getUser != null) {
         QueryGetUserWithId$getUser data = result.parsedData!.getUser!;
-        return SingleChildScrollView(
-          physics: const NeverScrollableScrollPhysics(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 8.w),
-              SizedBox(
-                height: 29.w,
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    data.name,
-                    style: TextStyle(
-                      fontSize: 22.sp,
-                      fontWeight: FontWeight.w400,
-                      color: kPrimaryBlack,
+        return Opacity(
+          opacity: 1 - scrollPercent,
+          child: SingleChildScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 8.w),
+                SizedBox(
+                  height: 29.w,
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      data.name,
+                      style: TextStyle(
+                        fontSize: 22.sp,
+                        fontWeight: FontWeight.w400,
+                        color: kPrimaryBlack,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 22.w,
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    data.email,
-                    style: TextStyle(
-                      fontSize: 12.sp,
-                      color: kDeepBlue2,
-                      fontWeight: FontWeight.w400,
-                      height: 1,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: expandedHeight -
-                    topBarHeight -
-                    tabHeight -
-                    indicatorWeight -
-                    88.w,
-                child: AutoSizeText(
-                  data.bio!,
-                  minFontSize: 9.sp,
-                  stepGranularity: 1.sp,
-                  maxFontSize: 12.sp,
-                  maxLines: 7,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    color: kPrimaryBlack,
-                    fontWeight: FontWeight.w300,
-                    height: 1,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 15.w,
-                child: Row(
-                  children: [
-                    Text(
-                      "Message me on: ",
+                SizedBox(
+                  height: 22.w,
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      data.email,
                       style: TextStyle(
                         fontSize: 12.sp,
-                        color: kPrimaryBlack,
+                        color: kDeepBlue2,
                         fontWeight: FontWeight.w400,
                         height: 1,
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 3.w),
-                      child: FaIcon(
-                        FontAwesomeIcons.solidEnvelope,
-                        size: 15.w,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 3.w),
-                      child: FaIcon(
-                        FontAwesomeIcons.twitter,
-                        size: 15.w,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 11.w),
-            ],
+                SizedBox(
+                  height: expandedHeight -
+                      topBarHeight -
+                      tabHeight -
+                      indicatorWeight -
+                      88.w,
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: IgnorePointer(
+                          ignoring: true,
+                          child: ReadMoreText(
+                            data.bio ?? "",
+                            trimLines: 3,
+                            style: TextStyle(
+                              fontSize: 12.sp,
+                              color: kPrimaryBlack,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            colorClickableText: Colors.grey,
+                            trimMode: TrimMode.Line,
+                            trimCollapsedText: 'read more >>',
+                            delimiter: '... ',
+                            moreStyle: TextStyle(
+                              fontSize: 12.sp,
+                              color: kDeepBlue2,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        child: GestureDetector(
+                          onTap: () {
+                            showToast('bio clicked');
+                          },
+                          child: Container(
+                            color: Colors.transparent,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 15.w,
+                  child: Row(
+                    children: [
+                      Text(
+                        "Message me on: ",
+                        style: TextStyle(
+                          fontSize: 12.sp,
+                          color: kPrimaryBlack,
+                          fontWeight: FontWeight.w400,
+                          height: 1,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 3.w),
+                        child: FaIcon(
+                          FontAwesomeIcons.solidEnvelope,
+                          size: 15.w,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 3.w),
+                        child: FaIcon(
+                          FontAwesomeIcons.twitter,
+                          size: 15.w,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 11.w),
+              ],
+            ),
           ),
         );
       } else {
